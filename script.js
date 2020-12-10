6ce1a368d4aa8e7964aa9cc04559f94e2f7c16d5
james lavis <RealHydraElite@gmail.com>
2020-12-10 19:16:11 +0000

    i deleted the alerts and add more id's for the p so it will be easyer to read for the user so they dont have to close the alert tab and also changed the background color to black and the text to yellow

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
+
+//the number is to long sometimes.
+//it doesnt round up.
 function basicMath(){
   var num1=Number(prompt("what is the first number"));
   var num2=Number(prompt("what is the second number"));
   var witch=prompt("what are you doing");
   if(witch=="adding"){
     var adding=num1+num2;
-    document.getElementById("endnum").innerHTML =adding;
+    document.getElementById("adding").innerHTML =adding;
   }else if(witch=="sub"){
     var subtraction=num1-num2;
-    document.getElementById("endnum").innerHTML = subtraction;
-    alert(subtraction);
+    document.getElementById("sub").innerHTML = subtraction;
   }else if(witch=="multi"){
-    var multiplcation=num1*num2;
-    document.getElementById("endnum").innerHTML = multiplcation;
-    alert(multiplcation);
+    var multiplication=num1*num2;
+    document.getElementById("multi").innerHTML = multiplication;
   }else if(witch=="divid"){
     var division=num1/num2;
-    document.getElementById("endnum").innerHTML =division;
-    alert(division);
+    document.getElementById("divid").innerHTML =division;
   }
 }
